858ae0fa7fcc9741b3513dafcdfb7d2ece3e24a1
github username <github email address>
2018-11-06 17:48:40 +0000

    Done.

--- src/components/CocktailsContainer.js
+++ src/components/CocktailsContainer.js
@@ -5,11 +5,34 @@ import Form from './Form'
 
 class CocktailsContainer extends Component {
 
+  state = {
+    cocktails: [],
+    currentCocktail: null
+  }
+
+  getCocktails = () => {
+    return fetch('https://react-boozer-backend.herokuapp.com/api/v1/cocktails')
+    .then(resp => resp.json())
+    .then(resp => this.setState({cocktails: resp}))
+  }
+
+  componentDidMount() {
+    this.getCocktails()
+  }
+
+  setCurrentCocktail = (cocktail) => {
+    {this.setState({currentCocktail: cocktail})} 
+  }
+
   render(){
     return (
       <div className="container">
-      
+      <CocktailsList cocktails={this.state.cocktails} setCurrentCocktail={this.setCurrentCocktail} />
+      <CocktailDisplay cocktail={this.state.currentCocktail}/>
+      <Form />
       </div>
+      
+      
     )
   }
 }
